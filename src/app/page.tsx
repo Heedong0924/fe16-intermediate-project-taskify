07b840991394f50e6d291b56bc04fbad59ae0acf
Heedong <donghui11777@gmail.com>
2025-07-18 00:29:04 +0900

    LIN-11 feat: PR Preview 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>Welcome to My Next.js App!</h1>
       <p>Start building your amazing application.</p>
+      <h1 className="text-red-600">PR PREVIEW TEST</h1>
     </main>
   );
 }
