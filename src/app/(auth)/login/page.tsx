e06f93cce9bc33ec19d19e7efe2ec5895f641691
Hwigeon <bb7788@naver.com>
2025-07-21 16:24:58 +0900

    LIN-23 feat: 로그인 유효성 검증 확인

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,46 @@
+'use client';
+
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 import Input from '@/components/common/Input';
+import { emailValidation, passwordValidation } from '@/lib/validationRules';
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data); // 사용자가 입력한 값
+  };
+
   return (
     <div className="flex w-full flex-col items-center justify-center gap-6">
-      <form className="flex w-full flex-col items-center justify-center gap-6">
+      <form
+        className="flex w-full flex-col items-center justify-center gap-6"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="flex w-full flex-col items-center justify-center gap-4">
-          <Input label="이메일" type="email" />
-          <Input label="비밀번호" type="password" />
+          <Input
+            label="이메일"
+            type="email"
+            isError={!!errors.email}
+            {...register('email', emailValidation)}
+          />
+          <Input
+            label="비밀번호"
+            type="password"
+            isError={!!errors.password}
+            {...register('password', passwordValidation)}
+          />
         </div>
         <button
           className="bg-violet-primary w-full rounded-lg py-2 text-white"
