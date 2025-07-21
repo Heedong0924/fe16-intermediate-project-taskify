571235b10ce3eb89c928b73620c1473834ff3f6f
Hwigeon <bb7788@naver.com>
2025-07-21 17:18:53 +0900

    LIN-23 fix: 이미지 관련 경고 수정

--- src/app/(auth)/components/AuthHero.tsx
+++ src/app/(auth)/components/AuthHero.tsx
@@ -5,14 +5,14 @@ const AuthHero = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center gap-[10px]">
-        <div className="flex flex-col items-center justify-between gap-[30px]">
+        <div className="w-max-[200px] flex w-full flex-col items-center justify-between gap-[30px]">
           <Image
             src="/images/LogoImage.svg"
             alt="로고"
             width={200}
             height={200}
             priority
-            className="h-auto w-auto"
+            style={{ width: '200px', height: 'auto' }}
           />
           <Image
             src="/images/Taskify.svg"
@@ -20,7 +20,7 @@ const AuthHero = () => {
             width={200}
             height={50}
             priority
-            className="h-auto w-auto"
+            style={{ width: '200px', height: 'auto' }}
           />
         </div>
         <h3 className="text-center text-xl">오늘도 만나서 반가워요 !</h3>
