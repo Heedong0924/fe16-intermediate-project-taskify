92d626e394d61869cb2a12919865cc7eefafaf23
Sophia Na (나소연) <devsophia029@gmail.com>
2025-07-22 11:01:17 +0900

    LIN-22 feat: 서치박스 및 서치박스 인풋 추가

--- src/app/mydashboard/components/invitationList/SearchBox.tsx
+++ src/app/mydashboard/components/invitationList/SearchBox.tsx
@@ -1,29 +1,38 @@
-// 'use client'
+'use client';
 
-// import { useDebounce } from "@/hooks/useDebounce";
-// import { useGetInvitations } from "@/hooks/useGetInvitations";
-// import { useMemo, useState } from "react"
-// import SearchInput from "./SearchInput";
+import { useMemo, useState } from 'react';
 
-// const SearchBox = () => {
-//   const [searchTerm, setSearchTerm] = useState('');
+import { useDebounce } from '@/hooks/useDebounce';
+import { useGetInvitations } from '@/hooks/useGetInvitations';
 
-//   // 디바운스 시간은 300ms로 사용
-//   const debouncedSearchTerm = useDebounce(searchTerm, 300);
+import SearchInput from './SearchInput';
 
-//   const { data } = useGetInvitations();
+const SearchBox = () => {
+  const [searchTerm, setSearchTerm] = useState('');
 
-//   // data 또는 debouncedSearchTerm이 바뀔 때만 필터 연산 수행 (불필요한 연산 방지)
-//   // const filteredResults = useMemo(()=> { if (!data?.invitations) return [];
-//   //   return data.invitations.filter((item)=> item.dashboard.title.toLowerCase().includes(debouncedSearchTerm.toLowerCase()));
-//   // }, [data, debouncedSearchTerm]);
+  // 디바운스 시간은 300ms로 사용
+  const debouncedSearchTerm = useDebounce(searchTerm, 300);
 
-//   // 추후 리스트 렌더링 시 filteredResults 연결하여 활용 예정
-//   return (
-//     <div>
-//       <SearchInput value={searchTerm} onChange={setSearchTerm} />
-//     </div>
-//   )
-// }
+  const { data } = useGetInvitations();
 
-// export default SearchBox
+  // data 또는 debouncedSearchTerm이 바뀔 때만 필터 연산 수행 (불필요한 연산 방지)
+  const filteredResults = useMemo(() => {
+    if (!data?.invitations) return [];
+    return data.invitations.filter((item) =>
+      item.dashboard.title
+        .toLowerCase()
+        .includes(debouncedSearchTerm.toLowerCase()),
+    );
+  }, [data, debouncedSearchTerm]);
+
+  // 추후 리스트 렌더링 시 filteredResults 연결하여 활용 예정
+  console.log(filteredResults);
+
+  return (
+    <div>
+      <SearchInput value={searchTerm} onChange={setSearchTerm} />
+    </div>
+  );
+};
+
+export default SearchBox;
--- src/app/mydashboard/components/invitationList/SearchInput.tsx
+++ src/app/mydashboard/components/invitationList/SearchInput.tsx
@@ -1,17 +1,25 @@
-// import { ChangeEvent } from "react";
+import { ChangeEvent } from 'react';
 
-// interface SearchInputProps {
-//   value: string;
-//   onChange: (value:string) => void;
-// }
+interface SearchInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
 
-// const SearchInput = ({value, onChange}:SearchInputProps) => {
-//   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {onChange(e.target.value);};
+const SearchInput = ({ value, onChange }: SearchInputProps) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+  console.log(value);
+  // css 예시. 수정 필요
+  return (
+    <input
+      type="text"
+      placeholder="초대받은 프로젝트 이름을 검색하세요"
+      value={value}
+      onChange={handleChange}
+      className="h-6 w-90"
+    />
+  );
+};
 
-//   // css 예시. 수정 필요
-//   return (
-//     <input type="text" placeholder="초대받은 프로젝트 이름을 검색하세요" value={value} onChange={handleChange} className="w-90 h-6" />
-//   )
-// }
-
-// export default SearchInput
+export default SearchInput;
