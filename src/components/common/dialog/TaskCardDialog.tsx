45682e120ca109e16f4efa8545064009ba51dfc5
Heedong <donghui11777@gmail.com>
2025-07-27 00:01:32 +0900

    LIN-31 style: TaskCardDialog Skeleton UI 추가 + useSkeleton 추가

--- src/components/common/dialog/TaskCardDialog.tsx
+++ src/components/common/dialog/TaskCardDialog.tsx
@@ -3,10 +3,14 @@ import Image from 'next/image';
 
 import {
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/Dialog';
+import SkeletonLine from '@/components/ui/SkeletonLIne';
+import SkeletonParagraph from '@/components/ui/SkeletonParagraph';
+import { useSkeleton } from '@/hooks/useSkeleton';
 import { getCard } from '@/lib/api/cardService';
 
 import CardAuthor from './dialog-components/CardAuthor';
@@ -28,51 +32,82 @@ const TaskCardDialog = ({
   cardId,
   columnName,
 }: TaskCardDialogProps) => {
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['detailCard', cardId],
     queryFn: () => getCard(cardId),
   });
 
+  const { showSkeleton, isFadingOut } = useSkeleton(isLoading, 1000);
+
   return (
     <DialogContent
-      className="dialog-scrollable-content h-[50vh] max-h-[710px] max-w-[327px] gap-4 overflow-auto px-4 md:max-h-[766px] md:max-w-[678px] md:gap-6 md:px-8 lg:max-w-[732px]"
+      className="md:grid-cols-[repeat(4, 1fr)_181px] dialog-scrollable-content grid h-[50vh] max-h-[710px] max-w-[327px] grid-cols-4 gap-4 overflow-auto px-4 md:max-h-[766px] md:max-w-[678px] md:gap-6 md:px-8 lg:max-w-[732px]"
       showCloseButton={false}
       onOpenAutoFocus={(event) => event.preventDefault()}
     >
       {/** Header 영역 */}
-      <DialogHeader className="col-start-1 col-end-5 flex gap-4 md:gap-6">
+      <DialogHeader className="col-start-1 col-end-5 flex gap-0">
         <TaskCardDialogControlArea
           className="absolute top-5 right-5 flex gap-5"
           cardId={cardId}
         />
-        <DialogTitle className="mt-12 text-left md:mt-0">
-          <span className="text-taskify-2xl-bold text-taskify-neutral-700">
-            {data?.title}
-          </span>
-        </DialogTitle>
+        {showSkeleton ? (
+          <SkeletonLine
+            className="mt-12 h-8 w-4/5 md:mt-0"
+            isFadingOut={isFadingOut}
+          />
+        ) : (
+          <DialogTitle className="mt-12 text-left md:mt-0">
+            <span className="text-taskify-2xl-bold text-taskify-neutral-700">
+              {data?.title}
+            </span>
+          </DialogTitle>
+        )}
+        <DialogDescription />
       </DialogHeader>
       {/* CardAuthor 영역 */}
-      <CardAuthor
-        className="border-taskify-neutral-300 col-start-1 col-end-5 rounded-lg border-1 md:col-start-5 md:col-end-7 md:row-span-3 md:mt-15 md:h-[155px] md:w-[181px]"
-        author={data?.assignee.nickname}
-        dueDate={data?.dueDate}
-      />
+      {showSkeleton ? (
+        <SkeletonLine
+          className="col-start-1 col-end-5 h-[72px] w-full md:col-start-5 md:col-end-6 md:row-span-3 md:mt-15 md:h-[155px] md:w-[181px]"
+          isFadingOut={isFadingOut}
+        />
+      ) : (
+        <CardAuthor
+          className="border-taskify-neutral-300 col-start-1 col-end-5 rounded-lg border-1 md:col-start-5 md:col-end-6 md:row-span-3 md:mt-15 md:h-[155px] md:w-[181px]"
+          author={data?.assignee.nickname}
+          dueDate={data?.dueDate}
+        />
+      )}
 
-      <TaskCardChipStatus
-        className="col-start-1 col-end-5 flex flex-wrap gap-2"
-        columnName={columnName}
-        tags={data?.tags}
-      />
+      {showSkeleton ? (
+        <SkeletonLine
+          className="col-start-1 col-end-5 h-[27px] w-3/5"
+          isFadingOut={isFadingOut}
+        />
+      ) : (
+        <TaskCardChipStatus
+          className="col-start-1 col-end-5 flex flex-wrap gap-2"
+          columnName={columnName}
+          tags={data?.tags}
+        />
+      )}
 
       {/** Contents 영역 */}
-      <div className="col-start-1 col-end-5 gap-4">
-        <div className="text-taskify-xs-normal md:text-taskify-md-regular">
-          {data?.description}
-        </div>
-        <div className="mt-4 mb-2">
+      {showSkeleton ? (
+        <SkeletonParagraph
+          paragraphClassName="w-full col-start-1 col-end-5 gap-[6px] md:gap-[10px]"
+          lineClassName="h-[12px] md:h-[14px]"
+          lines={8}
+          isFadingOut={isFadingOut}
+        />
+      ) : (
+        <div className="col-start-1 col-end-5 gap-4">
+          <div className="text-taskify-xs-normal md:text-taskify-md-regular">
+            {data?.description}
+          </div>
           {data?.imageUrl && (
             <Image
-              className="overflow-hidden rounded-lg"
+              className="mt-4 mb-2 overflow-hidden rounded-lg"
               src="https://picsum.photos/id/684/600/400"
               alt="test"
               width={600}
@@ -80,18 +115,27 @@ const TaskCardDialog = ({
             />
           )}
         </div>
-      </div>
+      )}
 
       {/* Footer 영역 (Comments) */}
-      <DialogFooter className="col-start-1 col-end-5 flex flex-col gap-4 sm:flex-col md:flex-col">
-        <TaskDialogCreateCommentForm
-          className="relative flex flex-col justify-between"
-          dashboardId={dashboardId}
-          columnId={columnId}
-          cardId={cardId}
+      {showSkeleton ? (
+        <SkeletonParagraph
+          paragraphClassName="col-start-1 col-end-5 gap-4"
+          lineClassName="h-[48px]"
+          lines={3}
+          isFadingOut={isFadingOut}
         />
-        {data?.id && <Comments cardId={data!.id} />}
-      </DialogFooter>
+      ) : (
+        <DialogFooter className="col-start-1 col-end-5 flex flex-col gap-4 sm:flex-col md:flex-col">
+          <TaskDialogCreateCommentForm
+            className="relative flex flex-col justify-between"
+            dashboardId={dashboardId}
+            columnId={columnId}
+            cardId={cardId}
+          />
+          {data?.id && <Comments cardId={data!.id} />}
+        </DialogFooter>
+      )}
     </DialogContent>
   );
 };
