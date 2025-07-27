3a2f05affeefcf119e0a0e3bc3ff6d958a69d1a7
Sophia Na (나소연) <devsophia029@gmail.com>
2025-07-27 20:26:35 +0900

    LIN-55 feat: 대시보드 상세페이지 레이아웃 내 헤더 추가

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import Header from '@/components/common/header/Header';
 import Sidebar from '@/components/common/Sidebar';
 
 export const metadata = {
-  title: 'Dashboard | My Next.js App',
-  description: 'Dashboard specific layout',
+  title: 'Dashboard | Taskify',
+  description: '대시보드 상세 페이지입니다.',
 };
 
 export default function DashboardLayout({
@@ -15,7 +16,10 @@ export default function DashboardLayout({
   return (
     <div className="relative flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 md:ml-[160px] xl:ml-[300px]">{children}</main>
+      <Header />
+      <main className="flex-1 md:mt-[90px] md:ml-[160px] xl:ml-[300px]">
+        {children}
+      </main>
     </div>
   );
 }
