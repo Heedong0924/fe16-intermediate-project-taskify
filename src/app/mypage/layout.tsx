e38198ef41470f2ecf3df791d079f376ee2ac6b1
Sophia Na (나소연) <devsophia029@gmail.com>
2025-07-27 20:28:54 +0900

    LIN-55 feat: 마이페이지 레이아웃 내 헤더 추가 및 메타데이터 수정

---
+++ src/app/mypage/layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Header from '@/components/common/header/Header';
+import Sidebar from '@/components/common/Sidebar';
+
+export const metadata = {
+  title: 'My Page | Taskify',
+  description:
+    '내 정보, 비밀번호 변경, 프로필 사진을 설정할 수 있는 마이페이지입니다.',
+};
+
+export default function MypageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex min-h-screen">
+      <Sidebar />
+      <Header />
+      <main className="flex-1 md:mt-[90px] md:ml-[160px] xl:ml-[300px]">
+        {children}
+      </main>
+    </div>
+  );
+}
