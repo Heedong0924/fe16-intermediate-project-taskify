3c287e6efdd04fdd8433a48646124c0a75f09900
Sophia Na (나소연) <devsophia029@gmail.com>
2025-07-29 02:08:53 +0900

    LIN-65 feat: 문구 출력 조건 변경

--- src/app/mydashboard/components/dashboard/MemberCountText.tsx
+++ src/app/mydashboard/components/dashboard/MemberCountText.tsx
@@ -1,6 +1,6 @@
 const MemberCountText = ({ count }: { count: number }) => {
   // 혼자만 있는 대시보드의 경우 함께할 동료를 추가하라는 메시지 출력
-  if (count === 0) {
+  if (count === 1) {
     return (
       <p className="text-taskify-xs-normal text-taskify-neutral-400 whitespace-nowrap">
         함께할 동료를 추가해보세요
@@ -8,7 +8,7 @@ const MemberCountText = ({ count }: { count: number }) => {
     );
   }
 
-  // 한 명 이상 있을 경우 총 참여중인 사람 수를 출력
+  // 두 명 이상 있을 경우 총 참여중인 사람 수를 출력
   return (
     <p className="text-taskify-xs-normal text-taskify-neutral-400 whitespace-nowrap">
       <span className="text-taskify-xs-medium text-taskify-neutral-500">
