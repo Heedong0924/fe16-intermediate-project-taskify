99945e956307de16a24c5dc942fddbaaa0580b7a
10000jaeN <akswo256@naver.com>
2025-07-29 17:05:46 +0900

    LIN-64 refactor: 대시보드 수정페이지 스크롤 문제 해결, 레이아웃 기본배경색 설정

--- src/app/dashboard/[dashboardId]/edit/page.tsx
+++ src/app/dashboard/[dashboardId]/edit/page.tsx
@@ -6,6 +6,7 @@ import { notFound, useParams } from 'next/navigation';
 
 import ConfirmDashboardDeletionDialog from '@/components/common/dialog/ConfirmDashboardDeletionDialog';
 import Button from '@/components/ui/Buttons';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { useDialogStore } from '@/stores/useDialogStore';
 
 import DashboardInvitations from './components/DashboardInvitations';
@@ -24,46 +25,49 @@ export default function DashboardEditPage() {
   }
 
   return (
-    <div className="min-h-screen bg-[var(--gray-FAFAFA)] p-[20px]">
-      {/* 뒤로가기 */}
-      <nav className="mb-[10px] sm:mb-[25px]">
-        <Link
-          href={`/dashboard/${dashboardId}`}
-          className="flex items-center gap-2"
+    <ScrollArea className="overflow-auto">
+      <div className="min-h-screen bg-[var(--gray-FAFAFA)] p-[20px]">
+        {/* 뒤로가기 */}
+        <nav className="mb-[10px] sm:mb-[25px]">
+          <Link
+            href={`/dashboard/${dashboardId}`}
+            className="flex items-center gap-2"
+          >
+            <Image
+              src="/images/arrow-left.svg"
+              alt="돌아가기"
+              width={20}
+              height={20}
+            />
+            돌아가기
+          </Link>
+        </nav>
+
+        {/* 대시보드 타이틀, 컬러 수정 */}
+        <DashboardUpdate dashboardId={id} />
+
+        {/* 구성원 */}
+        <DashboardMembers dashboardId={id} />
+
+        {/* 초대 내역 */}
+        <DashboardInvitations dashboardId={id} />
+
+        {/*  대시보드 삭제하기 */}
+        <Button
+          color="white-black"
+          className="btn-removeDash w-full border border-[#D9D9D9] bg-transparent sm:w-[320px]"
+          onClick={() => {
+            openDialog({
+              dialogComponent: (
+                <ConfirmDashboardDeletionDialog dashboardId={id} />
+              ),
+            });
+          }}
         >
-          <Image
-            src="/images/arrow-left.svg"
-            alt="돌아가기"
-            width={20}
-            height={20}
-          />
-          돌아가기
-        </Link>
-      </nav>
-
-      {/* 대시보드 타이틀, 컬러 수정 */}
-      <DashboardUpdate dashboardId={id} />
-
-      {/* 구성원 */}
-      <DashboardMembers dashboardId={id} />
-
-      {/* 초대 내역 */}
-      <DashboardInvitations dashboardId={id} />
-
-      {/*  대시보드 삭제하기 */}
-      <Button
-        color="white-black"
-        className="btn-removeDash w-full border border-[#D9D9D9] bg-transparent sm:w-[320px]"
-        onClick={() => {
-          openDialog({
-            dialogComponent: (
-              <ConfirmDashboardDeletionDialog dashboardId={id} />
-            ),
-          });
-        }}
-      >
-        대시보드 삭제하기
-      </Button>
-    </div>
+          대시보드 삭제하기
+        </Button>
+      </div>
+      <ScrollBar className="hidden" />
+    </ScrollArea>
   );
 }
